2ea4fcc16afeece7ddebc89509413b6832e07fa6
MichalSurowka <msurowka9@gmail.com>
2022-07-06 18:57:54 +0200

    set up "hover" effect
    in for loop was used gridElement.cloneNode(true) because
    it is possible to add new Element only once
    
    event listener was added in loop bc cloning doesnt clone
    evenet listeners from cloned element

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let gridContainer = document.querySelector('#grid-container');
-
 window.onload = () => {
     let gridElement = document.createElement('div');
     gridElement.style.cssText = "width: 40px;height:40px;flex: 1 0 auto;";
-    gridContainer.appendChild(gridElement);
-    for(let i = 0; i < 16*16-1; i++){
-        gridContainer.appendChild(gridElement.cloneNode(true));
+    for(let i = 0; i < 16*16; i++){
+        gridElementClone = gridElement.cloneNode(true);
+        gridElementClone.addEventListener('mouseover', (e) =>{
+            e.target.style.background = 'black';
+           });
+        gridContainer.appendChild(gridElementClone);
         console.log(i);
     }
 }
